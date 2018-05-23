bf5e92c88cb20ec03ba0b25529074d35cd83b1cf
Nathan Hill <nateohill@gmail.com>
2018-05-23 15:23:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz num
-  if num / 3 && !(num / 5)
+  if (num / 3 && !(num / 5))
     puts "Fizz"
   end
-  if num / 5 && !(num / 3)
+  if (num / 5 && !(num / 3))
     puts "Buzz"
   end
-  if num / 3 && num / 5
+  if (num / 3 && num / 5)
     puts "Fizz Buzz"
   else
     puts nil
